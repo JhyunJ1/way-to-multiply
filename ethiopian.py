3d228284bd26d7748cfd77f101a4da720367d379
sujinjwa <nasujin744@naver.com>
2023-01-12 10:13:40 +0900

    feat: Create function to multiply two numbers

--- ethiopian.py
+++ ethiopian.py
@@ -1,5 +1,25 @@
-def hello():
-    print('Good lock!!')
+def multiply_two_numbers(num1, num2):
+    origin_num1 = num1
+    origin_num2 = num2
+
+    result = 0
+    while(num1 > 1):
+        num1 //= 2
+        num2 *= 2
+        
+        if num1 == 0:
+            break
+
+        if num1 == 1:
+            result += num2
+            break
+        
+        if num1 % 2 == 1:
+            result += num2
+
+    print(f'{origin_num1} x {origin_num2} =', result)
+
+num1, num2 = list(map(int, input('Enter Two Numbers with blank: ').split()))
 
 if __name__=='__main__':
-    hello()
+    multiply_two_numbers(num1, num2)
